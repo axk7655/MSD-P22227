01cb8d21ce9090e33860daa1ff827583dc92d6fc
axk7655 <35964393+axk7655@users.noreply.github.com>
2021-11-03 17:19:45 -0400

    Added test code and started proto

---
+++ SRC/Main_Poto/Main_Poto.ino
@@ -0,0 +1,22 @@
+
+/*
+ * File: Main_Proto
+ * Description: Used during the prototype phase to test getting data from strain gauge
+ * Date: 2021-11-03
+ * Author: Andy Keats (axk7655@rit.edu)
+ * 
+ */
+void setup() {
+    Serial.begin(115200);               //initial the Serial
+    pinMode(A3, INPUT);
+}
+ 
+int main(void)
+{
+  while (true){
+    if(Serial.available())
+    {
+        Serial.write(analogRead(A3));    //send what has been received
+    }
+  }
+}  
