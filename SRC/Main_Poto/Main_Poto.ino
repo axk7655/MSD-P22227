3c430490133121f9f463e4c27646252a30f6b2e0
axk7655 <35964393+axk7655@users.noreply.github.com>
2021-11-06 18:44:31 -0400

    Added library for HX711 and support code
    Should print out to the terminal from either onboard ADC or the Amp/ADC board.

--- SRC/Main_Poto/Main_Poto.ino
+++ SRC/Main_Poto/Main_Poto.ino
@@ -6,17 +6,33 @@
  * Author: Andy Keats (axk7655@rit.edu)
  * 
  */
+
+#include "HX711.h"
+
+#define cal_factor 0
+
+#define DOUT  3
+#define CLK  2
+
+HX711 scale;
+ 
 void setup() {
     Serial.begin(115200);               //initial the Serial
     pinMode(A3, INPUT);
+
+    scale.begin(DOUT, CLK);
+    scale.set_scale(cal_factor);
+    scale.tare();
 }
  
 void loop()
 {
   if(Serial.available())
   {
-    Serial.write(analogRead(A3));    //send ADC value over bluetooth
-    Serial.print(analogRead(A3);    //To the terminal
+    Serial.print(scale.get_units(), 1); // Gets from amp and ADC board
+   
+    Serial.write(analogRead(A3));    //send onboard ADC value over bluetooth
+    Serial.print(analogRead(A3));    //To the terminal
     Serial.write("\n");
   }
 }  
