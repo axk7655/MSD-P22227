e3f1df3548a9f5e08aa9d26ec6a196ec0d0c70f0
Andy K <35964393+axk7655@users.noreply.github.com>
2021-11-17 12:44:12 -0500

    Sending ADC data to phone over bluetooth.
    Modified Hello_World, which was the Bluno demo, to include the information for the ADC chip. Used the information learned in previous experiment to get raw ADC values from the HX711. This was then connected to the example Bluno app. Work was done to get the data in the right format and changing raw ADC values were seen on the phone and the serial terminal.

--- SRC/Hello_World/Hello_World.ino
+++ SRC/Hello_World/Hello_World.ino
@@ -1,11 +1,40 @@
+#include "HX711.h"
+
+#define calibration_factor 0.0 //-7050.0 //This value is obtained using the SparkFun_HX711_Calibration sketch
+#define offset_factor 0.0
+
+#define DOUT  5
+#define CLK  4
+
+HX711 scale;
+
 void setup() {
     Serial.begin(115200);               //initial the Serial
+
+    scale.begin(DOUT, CLK);
+    scale.set_scale(calibration_factor); //This value is obtained by using the SparkFun_HX711_Calibration sketch
+    scale.set_offset(offset_factor); 
+    //scale.tare(); //Assuming there is no weight on the scale at start up, reset the scale to 0
+
+    Serial.println("Setup done.");
+
+    long int t1 = 0;
+    long int t2 = 0;
+    int idx = 0;
+    int data = 0;
+    
 }
  
 void loop()
 {
+  int idx = 0;
     if(Serial.available())
     {
-        Serial.write(Serial.read());    //send what has been received
+        int data = scale.read();
+        //Serial.write(data);    //send over BLE 
+        Serial.println(data); //send over serial
+        //Serial.write(idx);
+        idx = idx + 1;
+        
     }
 } 
