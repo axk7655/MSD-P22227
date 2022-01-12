4903ce3ec6dfe9377e9825a4b32fd1356a02981e
Andy K <35964393+axk7655@users.noreply.github.com>
2022-01-12 12:55:02 -0500

    Made testing easier.
    Added try catch to not have to unplug and replug the device.

--- SRC/Test.m
+++ SRC/Test.m
@@ -3,13 +3,19 @@
 %Description: Testing to get data from the Bluno to MATLAB using the USB dongle.  
 %Author: Andy Keats
 
-ListOfPorts = serialportlist("available");
-arduinoObj = serialport("COM3",9600);
-configureTerminator(arduinoObj,"CR/LF");
-flush(arduinoObj);
-
+try
+    ListOfPorts = serialportlist("available");
+    arduinoObj = serialport("COM3",9600);
+    configureTerminator(arduinoObj,"CR/LF");
+    flush(arduinoObj);
+catch
+    disp("Already configured");
+end
+idx = 0;
 while true
     data = readline(arduinoObj);
 
     disp(data);
+    disp(idx);
+    idx = idx + 1;
 end
